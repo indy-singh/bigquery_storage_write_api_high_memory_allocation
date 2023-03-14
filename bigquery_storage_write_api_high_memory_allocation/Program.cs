369eea1e6c5278cda34c6be59097fa1809e867fa
indy-singh <1906778+indy-singh@users.noreply.github.com>
2023-03-14 10:59:45 +0000

    Throw together some random data that is repeatable.
    https://github.com/protocolbuffers/protobuf/issues/12217

--- bigquery_storage_write_api_high_memory_allocation/Program.cs
+++ bigquery_storage_write_api_high_memory_allocation/Program.cs
@@ -1,9 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.Net;
-using Google.Cloud.BigQuery.Storage.V1;
-using Google.Cloud.BigQuery.V2;
-using Google.Protobuf;
-using Google.Protobuf.Collections;
+using static bigquery_storage_write_api_high_memory_allocation.WatchtowerBigQueryModel;
 
 namespace bigquery_storage_write_api_high_memory_allocation;
 
@@ -15,6 +13,78 @@ public class Program
         // fix the random so we have a repeatability of data.
         var random = new Random(1987);
 
+        var list = new List<Tuple<Fields, Counters>>();
+        var now = DateTime.UtcNow;
+        var dayBucket = now.Date;
+        var guids = new List<Guid>();
+
+        for (int i = 0; i < 5000; i++)
+        {
+            var buffer = new byte[16];
+            random.NextBytes(buffer);
+            guids.Add(new Guid(buffer));
+        }
+
+        for (int x = 0; x < 30_000; x++)
+        {
+            var fields = new Fields
+            {
+                MinuteBucket = dayBucket.AddMinutes((int)now.TimeOfDay.TotalMinutes + random.Next(-2 +2)), //smear this around artificially
+                UserReference = guids[random.Next(0, guids.Count)].ToString(),
+                SystemId = 1,
+                ApplicationName = "ApplicationName",
+                RequestTypeName = "RequestTypeName",
+                StatusCode = 1,
+                Revision = 314246,
+                HostName = Environment.MachineName,
+                ExternalApplicationName = "ExternalApplicationName",
+                IpAddress = IPAddress.Any.ToString(),
+            };
+            var counters = new Counters
+            {
+                TotalDuration = random.Next(1, 1000),
+                TotalSquareDuration = random.Next(1, 1000),
+                RequestBytes = random.Next(1, 1000),
+                ResponseBytes = random.Next(1, 1000),
+                PgSessions = random.Next(1, 1000),
+                SqlSessions = random.Next(1, 1000),
+                PgStatements = random.Next(1, 1000),
+                SqlStatements = random.Next(1, 1000),
+                PgEntities = random.Next(1, 1000),
+                SqlEntities = random.Next(1, 1000),
+                CassandraStatements = random.Next(1, 1000),
+                Hits = random.Next(1, 1000),
+            };
+            list.Add(Tuple.Create(fields, counters));
+        }
+
+
+        var localConcurrentDictionary = new ConcurrentDictionary<WatchtowerBigQueryModel.Fields, WatchtowerBigQueryModel.Counters>();
+
+        foreach (var item in list)
+        {
+            (WatchtowerBigQueryModel.Fields key, WatchtowerBigQueryModel.Counters value) = item;
+
+            WatchtowerBigQueryModel.Counters UpdateValueFactory(WatchtowerBigQueryModel.Fields existingKey, WatchtowerBigQueryModel.Counters existingValue)
+            {
+                existingValue.TotalDuration += value.TotalDuration;
+                existingValue.TotalSquareDuration += value.TotalSquareDuration;
+                existingValue.RequestBytes += value.RequestBytes;
+                existingValue.ResponseBytes += value.ResponseBytes;
+                existingValue.PgSessions += value.PgSessions;
+                existingValue.SqlSessions += value.SqlSessions;
+                existingValue.PgStatements += value.PgStatements;
+                existingValue.SqlStatements += value.SqlStatements;
+                existingValue.PgEntities += value.PgEntities;
+                existingValue.SqlEntities += value.SqlEntities;
+                existingValue.CassandraStatements += value.CassandraStatements;
+                existingValue.Hits += value.Hits;
+                return existingValue;
+            }
+
+            localConcurrentDictionary.AddOrUpdate(key, value, UpdateValueFactory);
+        }
+
         //DoLegacyInsertTest();
         //DoProtobufInsertTest();
 
@@ -24,105 +94,7 @@ public class Program
 
         for (var index = 0; index <= GC.MaxGeneration; index++)
             Console.WriteLine($"Gen {index} collections: {GC.CollectionCount(index)}");
-    }
 
-    private static void DoLegacyInsertTest()
-    {
-        var records = CreateLegacyInsertList();
-
-        for (int i = 0; i < 1_000_000; i++)
-        {
-            // this isn't really a "fair" test as the list isn't evaluated until something enumerates over it
-            var rows = records.Select(x => new BigQueryInsertRow{ x });
-        }
-    }
-
-    private static void DoProtobufInsertTest()
-    {
-        var records = CreateLegacyInsertList();
-
-        for (int i = 0; i < 1_000_000; i++)
-        {
-            // this isn't really a "fair" test as the list isn't evaluated until something enumerates over it
-            var rows = records.Select(x => new BigQueryInsertRow { x });
-        }
-    }
-
-    private static List<WatchtowerRecord> CreateStorageWriteApiList()
-    {
-        var random = Random.Shared;
-        var records = new List<WatchtowerRecord>();
-
-        for (int i = 0; i < 3; i++)
-        {
-            records.Add(new WatchtowerRecord
-            {
-                // fields
-                MinuteBucket = (DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() + random.Next(1000, 9000)) * 1000,
-                UserReference = Guid.NewGuid().ToString(),
-                SystemId = random.Next(1, 10),
-                ApplicationName = "ApplicationName",
-                RequestTypeName = "RequestTypeName",
-                StatusCode = random.Next(101, 999),
-                Revision = random.Next(100000, 999999),
-                HostName = Environment.MachineName,
-                ExternalApplicationName = "ExternalApplicationName",
-                IpAddress = IPAddress.Any.ToString(),
-                // counters
-                TotalDuration = random.Next(1000, 9000),
-                TotalSquareDuration = random.Next(1000, 9000),
-                RequestBytes = random.Next(1000, 9000),
-                ResponseBytes = random.Next(1000, 9000),
-                PgSessions = random.Next(1000, 9000),
-                SqlSessions = random.Next(1000, 9000),
-                PgStatements = random.Next(1000, 9000),
-                SqlStatements = random.Next(1000, 9000),
-                PgEntities = random.Next(1000, 9000),
-                SqlEntities = random.Next(1000, 9000),
-                CassandraStatements = random.Next(1000, 9000),
-                Hits = random.Next(1, 100),
-            });
-        }
-
-        return records;
-    }
-
-    private static List<Dictionary<string, object>> CreateLegacyInsertList()
-    {
-        var random = Random.Shared;
-        var list = new List<Dictionary<string, object>>();
-
-        for (int i = 0; i < 3; i++)
-        {
-            list.Add(new Dictionary<string, object>
-            {
-                //fields
-                ["minute_bucket"] = (DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() + random.Next(1000, 9000)) * 1000,
-                ["user_reference"] = Guid.NewGuid().ToString(),
-                ["system_id"] = random.Next(1, 10),
-                ["application_name"] = "ApplicationName",
-                ["request_type_name"] = "RequestTypeName",
-                ["status_code"] = random.Next(101, 999),
-                ["revision"] = random.Next(100000, 999999),
-                ["host_name"] = Environment.MachineName,
-                ["external_application_name"] = "ExternalApplicationName",
-                ["ip_address"] = IPAddress.Any.ToString(),
-                //counters
-                ["total_duration"] = random.Next(1000, 9000),
-                ["total_square_duration"] = random.Next(1000, 9000),
-                ["request_bytes"] = random.Next(1000, 9000),
-                ["response_bytes"] = random.Next(1000, 9000),
-                ["pg_sessions"] = random.Next(1000, 9000),
-                ["sql_sessions"] = random.Next(1000, 9000),
-                ["pg_statements"] = random.Next(1000, 9000),
-                ["sql_statements"] = random.Next(1000, 9000),
-                ["pg_entities"] = random.Next(1000, 9000),
-                ["sql_entities"] = random.Next(1000, 9000),
-                ["cassandra_statements"] = random.Next(1000, 9000),
-                ["hits"] = random.Next(1, 100),
-            });
-        }
-
-        return list;
+        Console.WriteLine(localConcurrentDictionary.Count); // this should be around ~5,000 to be LIVE-like
     }
 }
